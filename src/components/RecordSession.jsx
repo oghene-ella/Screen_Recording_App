c359b30e60e414742084279857ff521ecc843454
oghene-ella <oghenekaro57@gmail.com>
2023-10-01 16:29:37 +0100

    record controller

--- src/components/RecordSession.jsx
+++ src/components/RecordSession.jsx
@@ -1,59 +1,44 @@
 import  { useState } from "react";
+import timer from "../assets/icons/timer.svg"
+import menu from "../assets/icons/controls.svg"
 
 const RecordSession = () => {
+	
 	const [isDragging, setIsDragging] = useState(false);
-	const [draggedItem, setDraggedItem] = useState(null);
+	const [position, setPosition] = useState({ x: 0, y: 0 });
 
-	const dragStart = (e) => {
+	const handleMouseDown = (e) => {
 		setIsDragging(true);
-		setDraggedItem(e.target);
-	};
+		const initialX = e.clientX - position.x;
+		const initialY = e.clientY - position.y;
 
-	const dragEnter = (e) => {
-		e.preventDefault();
-		if (!isDragging) return;
-		e.target.style.backgroundColor = "lightgray";
-	};
+		const handleMouseMove = (e) => {
+			const newX = e.clientX - initialX;
+			const newY = e.clientY - initialY;
+			setPosition({ x: newX, y: newY });
+		};
 
-	const dragLeave = (e) => {
-		e.target.style.backgroundColor = "";
-	};
-
-	const drop = (e) => {
-		e.preventDefault();
-		e.target.style.backgroundColor = "";
-		if (!draggedItem) return;
-
-		// Swap the positions of the dragged item and the target item
-		const container = e.target.parentElement;
-		const items = Array.from(container.children);
-		const draggedIndex = items.indexOf(draggedItem);
-		const targetIndex = items.indexOf(e.target);
-
-		if (draggedIndex !== -1 && targetIndex !== -1) {
-			// Insert the dragged item before the target item
-			if (draggedIndex < targetIndex) {
-				container.insertBefore(draggedItem, e.target.nextSibling);
-			} else {
-				container.insertBefore(draggedItem, e.target);
-			}
-
-			setDraggedItem(null);
+		const handleMouseUp = () => {
 			setIsDragging(false);
-		}
+			window.removeEventListener("mousemove", handleMouseMove);
+			window.removeEventListener("mouseup", handleMouseUp);
+		};
+
+		window.addEventListener("mousemove", handleMouseMove);
+		window.addEventListener("mouseup", handleMouseUp);
 	};
 
 	return (
-		<section className="w-full flex justify-center">
+		<section className="w-full flex justify-center items-end">
 			<main
-				className="border-8 border-zinc-200 w-1/2 h-[7rem] rounded-full bg-dark_black px-5 py-5 text-white"
-				draggable
-				onDragStart={dragStart}
-				onDragEnter={dragEnter}
-				onDragLeave={dragLeave}
-				onDrop={drop}
+				className={`border-8 border-zinc-200 w-1/2 h-[7rem] rounded-full bg-dark_black px-5 py-5 text-white flex gap-10 items-center draggable-element ${
+					isDragging ? "dragging" : ""
+				}`}
+				style={{ left: `${position.x}px`, top: `${position.y}px` }}
+				onMouseDown={handleMouseDown}
 			>
-				<h1>Hello</h1>
+				<img src={timer} />
+				<img src={menu} />
 			</main>
 		</section>
 	);
